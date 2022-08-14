698102aecaf96a197dfeffa37cf4f964bb2df5e7
kochenov <kochenof@gmail.com>
2022-08-14 09:25:53 +0300

    Добавил логопип на сайт

---
+++ src/components/parts/LeftPart.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="text-center">
+    <q-img
+      class="q-mt-md"
+      src="~assets/logo.png"
+      style="height: 84px; max-width: 180px"
+      loading="lazy"
+    />
+    <div class="text-subtitle2 text-weight-bold">УСАДЬБА МЕЧТЫ</div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
