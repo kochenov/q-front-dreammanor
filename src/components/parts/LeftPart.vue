fb093f0bcb740f0f7b136f42ea6af7de8ccf2faa
kochenov <kochenof@gmail.com>
2022-08-14 11:05:57 +0300

    add Left part component

--- src/components/parts/LeftPart.vue
+++ src/components/parts/LeftPart.vue
@@ -1,7 +1,67 @@
 <template>
-  <div class="text-center">
+  <q-scroll-area
+    class="q-pa-md"
+    style="
+      height: calc(100% - 165px);
+      margin-top: 165px;
+      border-right: 1px solid #ddd;
+    "
+  >
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil
+    praesentium molestias a adipisci, dolore vitae odit, quidem consequatur
+    optio voluptates asperiores pariatur eos numquam rerum delectus commodi
+    perferendis voluptate? Lorem ipsum dolor sit amet consectetur adipisicing
+    elit. Fugit nihil praesentium molestias a adipisci, dolore vitae odit,
+    quidem consequatur optio voluptates asperiores pariatur eos numquam rerum
+    delectus commodi perferendis voluptate? Lorem ipsum dolor sit amet
+    consectetur adipisicing elit. Fugit nihil praesentium molestias a adipisci,
+    dolore vitae odit, quidem consequatur optio voluptates asperiores pariatur
+    eos numquam rerum delectus commodi perferendis voluptate? Lorem ipsum dolor
+    sit amet consectetur adipisicing elit. Fugit nihil praesentium molestias a
+    adipisci, dolore vitae odit, quidem consequatur optio voluptates asperiores
+    pariatur eos numquam rerum delectus commodi perferendis voluptate? Lorem
+    ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil praesentium
+    molestias a adipisci, dolore vitae odit, quidem consequatur optio voluptates
+    asperiores pariatur eos numquam rerum delectus commodi perferendis
+    voluptate? Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+    nihil praesentium molestias a adipisci, dolore vitae odit, quidem
+    consequatur optio voluptates asperiores pariatur eos numquam rerum delectus
+    commodi perferendis voluptate? Lorem ipsum dolor sit amet consectetur
+    adipisicing elit. Fugit nihil praesentium molestias a adipisci, dolore vitae
+    odit, quidem consequatur optio voluptates asperiores pariatur eos numquam
+    rerum delectus commodi perferendis voluptate? Lorem ipsum dolor sit amet
+    consectetur adipisicing elit. Fugit nihil praesentium molestias a adipisci,
+    dolore vitae odit, quidem consequatur optio voluptates asperiores pariatur
+    eos numquam rerum delectus commodi perferendis voluptate? Lorem ipsum dolor
+    sit amet consectetur adipisicing elit. Fugit nihil praesentium molestias a
+    adipisci, dolore vitae odit, quidem consequatur optio voluptates asperiores
+    pariatur eos numquam rerum delectus commodi perferendis voluptate? Lorem
+    ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil praesentium
+    molestias a adipisci, dolore vitae odit, quidem consequatur optio voluptates
+    asperiores pariatur eos numquam rerum delectus commodi perferendis
+    voluptate? Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+    nihil praesentium molestias a adipisci, dolore vitae odit, quidem
+    consequatur optio voluptates asperiores pariatur eos numquam rerum delectus
+    commodi perferendis voluptate? Lorem ipsum dolor sit amet consectetur
+    adipisicing elit. Fugit nihil praesentium molestias a adipisci, dolore vitae
+    odit, quidem consequatur optio voluptates asperiores pariatur eos numquam
+    rerum delectus commodi perferendis voluptate? Lorem ipsum dolor sit amet
+    consectetur adipisicing elit. Fugit nihil praesentium molestias a adipisci,
+    dolore vitae odit, quidem consequatur optio voluptates asperiores pariatur
+    eos numquam rerum delectus commodi perferendis voluptate? Lorem ipsum dolor
+    sit amet consectetur adipisicing elit. Fugit nihil praesentium molestias a
+    adipisci, dolore vitae odit, quidem consequatur optio voluptates asperiores
+    pariatur eos numquam rerum delectus commodi perferendis voluptate? Lorem
+    ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil praesentium
+    molestias a adipisci, dolore vitae odit, quidem consequatur optio voluptates
+    asperiores pariatur eos numquam rerum delectus commodi perferendis
+    voluptate?
+  </q-scroll-area>
+  <div
+    class="absolute-top flex flex-center text-center column shadow-1 color-bg"
+    style="height: 165px"
+  >
     <q-img
-      class="q-mt-md"
       src="~assets/logo.png"
       style="height: 84px; max-width: 180px"
       loading="lazy"
