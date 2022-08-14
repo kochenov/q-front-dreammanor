c17143b96d8edfef04372e84dd6e9f73394555f3
kochenov <kochenof@gmail.com>
2022-08-14 09:51:17 +0300

    add компонент footer

---
+++ src/components/parts/FooterPart.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-toolbar>
+    <q-toolbar-title>
+      <div>{{ $route.name }}</div>
+    </q-toolbar-title>
+  </q-toolbar>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
